2a147c3515deb180f1821fdf9cd147acdbda97c4
Sumanth Naik <108412073+SNDevCodes@users.noreply.github.com>
2023-05-09 23:56:45 +0530

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,8 +3,14 @@ var count = 1;
 
 function fizzBuzz(){
 
-    if(count % 3 == 0){
+    if(count % 3 == 0 && count % 5 == 0) {
+        arr.push("FizzBuzz")
+    }
+    else if(count % 3 == 0){
         arr.push("Fizz");
+    }
+    else if (count % 5 == 0){
+        arr.push("Buzz");
     }
     else{
         arr.push(count);
